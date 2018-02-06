25110452c47d372273af0421c3c5a8b94e49fc72
yonggege <jamwong994@gmail.com>
2018-02-06 09:57:34 +0800

    Update log.py

--- log.py
+++ log.py
@@ -1,30 +1,39 @@
-# coding:utf-8
 import logging
+import os
+
+# 检查是否有log文件夹 没有则创建
+if not os.path.exists('log/'):
+    os.mkdir('log')
 
 # log setting
-LOG_FILE = 'test.log'
-ERROR_LOG_FILE = 'test_error.log'
+LOG_FILE = os.path.join('log', 'test.log')
+ERROR_LOG_FILE =  os.path.join('log', 'test_error.log')
 
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-
-logging.basicConfig(filename=LOG_FILE, filemode='a+',
-                    level=logging.INFO,
-                    format='%(asctime)s - %(levelname)s - %(message)s',
-                    )
-
+# 获取logger实例
 logger = logging.getLogger(__name__)
 
-# print in console
+# 指定logger输出格式
+formatter = logging.Formatter('[%(asctime)s] - %(levelname)s - %(message)s')
+
+# 文件日志
+file_handler = logging.FileHandler(LOG_FILE)
+file_handler.formatter = formatter
+logger.addHandler(file_handler)
+
+# 添加控制台logger
 console_handler = logging.StreamHandler()
 console_handler.formatter = formatter
 logger.addHandler(console_handler)
 
-# error file
-file_handler = logging.FileHandler(ERROR_LOG_FILE)
-file_handler.level = logging.ERROR
-file_handler.formatter = formatter
-logger.addHandler(file_handler)
+# 添加Error级别日志写入
+error_file_handler = logging.FileHandler(ERROR_LOG_FILE)
+error_file_handler.level = logging.ERROR
+error_file_handler.formatter = formatter
+logger.addHandler(error_file_handler)
+
+# 设置输出级别
+logger.setLevel(logging.INFO)
 
 logger.info('this is info')
-logger.warn('this is warn')
+logger.warning('this is warn')
 logger.error('this is error')
